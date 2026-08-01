164a7dd34859675b752dc339f0d5754e62aa05f0
Javier <schjavier@gmail.com>
2026-08-01 09:39:45 -0300

    test(messaging): complete MessagingValidationTest, plus refactor previous test to use TestDataFactory instead of Entity Builder methods

--- src/test/java/com/lotorojo/plugandchat/messaging/validations/MessagingValidationsTests.java
+++ src/test/java/com/lotorojo/plugandchat/messaging/validations/MessagingValidationsTests.java
@@ -1,14 +1,15 @@
 package com.lotorojo.plugandchat.messaging.validations;
 
 import com.lotorojo.plugandchat.TestDataFactory;
+import com.lotorojo.plugandchat.core.tenant.TenantContext;
 import com.lotorojo.plugandchat.identity.entity.UserAccount;
 import com.lotorojo.plugandchat.messaging.entity.Agent;
 import com.lotorojo.plugandchat.messaging.entity.Guest;
 import com.lotorojo.plugandchat.messaging.entity.Room;
+import com.lotorojo.plugandchat.messaging.entity.RoomStatus;
 import com.lotorojo.plugandchat.tenant.entity.Tenant;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.authentication.BadCredentialsException;
 
 import java.util.UUID;
@@ -26,9 +27,9 @@ public class MessagingValidationsTests {
 
     @Test
     public void shouldThrowExceptionWhenGuestIsNotTheOwner() {
-        Guest guestA = Guest.builder().uuid(UUID.randomUUID()).build();
-        Guest guestB = Guest.builder().uuid(UUID.randomUUID()).build();
-        Room roomA = Room.builder().guest(guestA).build();
+        Guest guestA = TestDataFactory.defaultGuest().toBuilder().uuid(UUID.randomUUID()).build();
+        Guest guestB = TestDataFactory.defaultGuest().toBuilder().uuid(UUID.randomUUID()).build();
+        Room roomA = TestDataFactory.defaultRoom().toBuilder().guest(guestA).build();
 
         assertThrows(BadCredentialsException.class, () ->
                 messagingValidations.validateGuestOwnsRoom(roomA, guestB));
@@ -37,20 +38,41 @@ public class MessagingValidationsTests {
 
     @Test
     public void shouldThrowExceptionWhenAgentTenantIsNotRoomTenant() {
-        Tenant tenant = Tenant.builder().uuid(UUID.randomUUID()).build();
-        UserAccount userAccount = UserAccount.builder().uuid(UUID.randomUUID()).tenant(tenant).build();
-        Agent agent = Agent.builder().uuid(UUID.randomUUID()).userAccount(userAccount).build();
+        Tenant tenant = TestDataFactory.defaultTenant().toBuilder().uuid(UUID.randomUUID()).build();
+        UserAccount userAccount = TestDataFactory.defaultUserAccount().toBuilder().uuid(UUID.randomUUID()).tenant(tenant).build();
+        Agent agent = TestDataFactory.defaultAgent().toBuilder().uuid(UUID.randomUUID()).userAccount(userAccount).build();
 
-        Tenant tenantB = Tenant.builder().uuid(UUID.randomUUID()).build();
-        UserAccount userAccountB = UserAccount.builder().tenant(tenantB).build();
-        Agent agentB = Agent.builder().uuid(UUID.randomUUID()).userAccount(userAccountB).build();
+        Tenant tenantB = TestDataFactory.defaultTenant().toBuilder().uuid(UUID.randomUUID()).build();
+        UserAccount userAccountB = TestDataFactory.defaultUserAccount().toBuilder().tenant(tenantB).build();
+        Agent agentB = TestDataFactory.defaultAgent().toBuilder().uuid(UUID.randomUUID()).userAccount(userAccountB).build();
 
 
-        Room roomA = Room.builder().tenant(tenant).agent(agent).build();
+        Room roomA = TestDataFactory.defaultRoom().toBuilder().tenant(tenant).agent(agent).build();
 
         assertThrows(BadCredentialsException.class, () ->
                 messagingValidations.agentTenantMatch(roomA, agentB));
-        
+
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenRoomIsClosed(){
+        Room room = TestDataFactory.defaultRoom().toBuilder().status(RoomStatus.CLOSED).build();
+
+        assertThrows(IllegalStateException.class, () ->
+                messagingValidations.validateRoomIsOpen(room));
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenRoomAndTenantUuidMismatch(){
+        Tenant tenant = TestDataFactory.defaultTenant().toBuilder().uuid(UUID.randomUUID()).build();
+        TenantContext.setCurrentTenant(tenant.getUuid());
+
+        Tenant tenantB = TestDataFactory.defaultTenant().toBuilder().uuid(UUID.randomUUID()).build();
+
+        Room room = TestDataFactory.defaultRoom().toBuilder().tenant(tenantB).build();
+
+        assertThrows(BadCredentialsException.class, () ->
+            messagingValidations.validateTenantAccess(room));
 
     }
 
