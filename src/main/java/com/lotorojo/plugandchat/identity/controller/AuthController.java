ca80978283f4672e5ef954ebd1ed42da16faa35e
Javier <schjavier@gmail.com>
2026-08-11 19:13:22 -0300

    feat(authentication): add methods for: - admin login -> this method return a httpOnly cookie with the jwt on it - admin logout -> this method rewrite the cookie with and empty string

--- src/main/java/com/lotorojo/plugandchat/identity/controller/AuthController.java
+++ src/main/java/com/lotorojo/plugandchat/identity/controller/AuthController.java
@@ -1,10 +1,14 @@
 package com.lotorojo.plugandchat.identity.controller;
 
 import com.lotorojo.plugandchat.core.security.JwtService;
+import com.lotorojo.plugandchat.identity.dto.LoginAdminResponse;
 import com.lotorojo.plugandchat.identity.dto.LoginRequest;
 import com.lotorojo.plugandchat.identity.dto.LoginResponse;
 import com.lotorojo.plugandchat.identity.service.AuthService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +27,43 @@ public class AuthController {
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
         return ResponseEntity.ok(authService.login(loginRequest));
 
+    }
+
+    @PostMapping("/login/admin")
+    public ResponseEntity<LoginAdminResponse> loginAdmin(@RequestBody LoginRequest loginRequest,
+                                                         HttpServletResponse response) {
+
+        LoginResponse loginResponse = authService.login(loginRequest);
+
+        ResponseCookie cookie = ResponseCookie.from("auth_token", loginResponse.token())
+                .httpOnly(true)
+                .secure(false)
+                .path("/")
+                .maxAge(86400)
+                .sameSite("lax")
+                .build();
+
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+
+        return ResponseEntity.ok(new LoginAdminResponse(
+                loginResponse.mail(),
+                "Authenticated"
+        ));
+
+    }
+
+    @PostMapping("logout")
+    public ResponseEntity<Void> logout(HttpServletResponse response) {
+        ResponseCookie cookie = ResponseCookie.from("auth_token", "")
+                .httpOnly(true)
+                .secure(false)
+                .path("/")
+                .maxAge(0)
+                .sameSite("lax")
+                .build();
+
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+        return ResponseEntity.noContent().build();
     }
 
 }
